e279d6175241d23de2d5147da3cbbaa46bb00493
andersononix <andersonkauan13@gmail.com>
2022-03-25 11:01:36 -0300

    Inicial commit

---
+++ Aulas/ex004.py
@@ -0,0 +1,7 @@
+q = (input('Digite algo: '))
+print(type(q))
+print(q.isalnum())
+print(q.isalpha())
+print(q.isascii())
+print(q.islower())
+
---
+++ Tetes/teste2.py
@@ -0,0 +1,9 @@
+"""
+nome=input("Seu nome: ")
+idade=input("Sua idade: ")
+perso=input("Seu perso: ")
+print(nome, idade, perso)
+"""
+nome=input("Digite seu nome: ")
+print("Prazer  em te conheser, "+nome+"!")
+
